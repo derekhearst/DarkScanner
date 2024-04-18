fe6e5d95f1014567740ffb0e0c4f4f79f1c09ea2
Derek Hearst <derekallenhearst@gmail.com>
2024-04-17 22:42:28 -0600

    Update rarities array in constants.ts to fix typo in 'Legend'

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const rarities = ['Junk', 'Poor', 'Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Unique']
+export const rarities = ['Junk', 'Poor', 'Common', 'Uncommon', 'Rare', 'Epic', 'Legend', 'Unique']
 
 export const slots = [
 	'Head',
